5fa302a875682aa7453d07a9a5732b5a03c85195
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-05-14 23:46:38 -0700

    chore: switch to fontkit
    Fontkit supports WOFF2 format

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,42 @@
-import * as opentype from "opentype.js";
+import fontkit, { Font } from "fontkit";
 
-const getLigatures = (font: opentype.Font): string[] => {
-  const { glyphIndexMap } = font.tables.cmap;
-  const glyphs = Object.entries(glyphIndexMap).reduce((obj, [key, value]) => {
-    return {
-      ...obj,
-      [String(value)]: key,
-    };
-  }, {});
-  const { ligatureSets, coverage } = font.tables.gsub.lookups[0].subtables[0];
-  const ligatures = ligatureSets.reduce((acc, set, index) => {
-    const firstChar = String.fromCharCode(glyphs[coverage.glyphs[index]]);
-    return [
+const getLigatures = (font: Font): string[] => {
+  const {
+    coverage: { rangeRecords },
+    ligatureSets,
+    // @ts-ignore
+  } = font.GSUB.lookupList.toArray()[0].subTables[0];
+
+  const leadingChars: string[] = rangeRecords.reduce(
+    (acc, { start, end }) => [
       ...acc,
-      ...set.map((ligature) => {
-        const name = ligature.components
-          .map((component) => String.fromCharCode(glyphs[component]))
-          .join("");
-        return firstChar + name;
-      }),
-    ];
-  }, []);
-  return ligatures;
+      ...Array.from(Array(end - start + 1), (_, x) => x + start).map(
+        (i) => font.stringsForGlyph(i)[0]
+      ),
+    ],
+    []
+  );
+
+  return ligatureSets.toArray().reduce(
+    (acc, ligatureSet, index) => [
+      ...acc,
+      ...ligatureSet.reduce((acc, ligature) => {
+        return [
+          ...acc,
+          leadingChars[index] +
+            ligature.components.map((x) => font.stringsForGlyph(x)[0]).join(""),
+        ];
+      }, []),
+    ],
+    []
+  );
 };
 
 export const getLigaturesFromPath = (
   fontFilePath: string
 ): Promise<string[]> => {
   return new Promise((resolve, reject) => {
-    opentype.load(fontFilePath, (err, font) => {
+    fontkit.open(fontFilePath, null, (err, font) => {
       if (err) {
         reject(err);
       }
@@ -41,9 +49,7 @@ export const getLigaturesFromPath = (
   });
 };
 
-export const getLigaturesFromBuffer = (
-  fontFileBuffer: ArrayBuffer
-): string[] => {
-  const font = opentype.parse(fontFileBuffer);
+export const getLigaturesFromBuffer = (fontFileBuffer: Buffer): string[] => {
+  const font = fontkit.create(fontFileBuffer);
   return getLigatures(font);
 };
